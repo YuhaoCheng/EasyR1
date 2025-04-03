bc2cfea2126e9deff6b048be8b5213dfb7267b3a
Yuhao Cheng <446358161@qq.com>
2025-04-03 06:47:09 +0000

    update the model path

--- examples/qwen2_5_vl_3b_geo3k_grpo.sh
+++ examples/qwen2_5_vl_3b_geo3k_grpo.sh
@@ -1,6 +1,6 @@
 set -x
 
-MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
+MODEL_PATH=/mnt/code/models/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
 
 SYSTEM_PROMPT="""You FIRST think about the reasoning process as an internal monologue and then provide the final answer.
  The reasoning process MUST BE enclosed within <think> </think> tags. The final answer MUST BE put in \boxed{}."""
@@ -14,4 +14,4 @@ python3 -m verl.trainer.main \
     worker.rollout.tensor_parallel_size=1 \
     worker.rollout.enable_chunked_prefill=false \
     trainer.experiment_name=qwen2_5_vl_3b_geo_grpo \
-    trainer.n_gpus_per_node=2
+    trainer.n_gpus_per_node=1
